751ee62fee1d169552634b30e63ec959b9adc554
Matyas420 <159123612+Matyas420@users.noreply.github.com>
2024-11-20 08:54:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import random
 
+# Vytvoření jednorozměrného pole s 10 náhodnými hodnotami
+random_array = [random.randint(0, 100) for _ in range(10)]
+
+# Jednoduché seřazení pole
+sorted_array = sorted(random_array)
+
+print(random_array)
+print(sorted_array)
